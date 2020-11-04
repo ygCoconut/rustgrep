d5a0144d2c23b19540af388259a9278bd4ddc984
ygCoconut <57194999+ygCoconut@users.noreply.github.com>
2020-11-04 13:47:03 +0100

    Finalized Project

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,49 @@
-use std::fs;   // read files
 use std::error;
+use std::fs; // read files
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         // remember 'static lifetimes live for the entire duration of the program, which is fine here.
         if args.len() < 3 {
-            return Err("Not enough arguments\ntry: rustgrep <query> <filename>")
+            return Err("Not enough arguments\ntry: rustgrep <query> <filename>");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        // clone allows Config to take ownership
-        // N.B.: cloning a full copy of the data gives up
-            // a little bit of performance, but makes the code
-            // way more readable.
-        Ok(Config {query, filename})
+    // UGLY CODE: OTHER WAY TO HANDLE OPTIONAL ARG BELOW
+    //     if args.len() == 4 {
+    //         // let case_sensitive = args[3].clone();
+    //         let case_sensitive = true;
+    //         Ok(Config { query, filename, case_sensitive })
+    //     }
+    //     else {
+    //         // clone allows Config to take ownership
+    //         // N.B.: cloning a full copy of the data gives up
+    //         // a little bit of performance, but makes the code
+    //         // way more readable.
+    //         let case_sensitive = false;
+    //         Ok(Config { query, filename, case_sensitive})
+    //     }
+    let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+    // env::var returns a Result
+    // by default case sensitive
+    // for case insensitivity, run:
+    // CASE_INSENSITIVE=1 cargo run to poem.txt
+    
+    Ok(Config {
+        query,
+        filename,
+        case_sensitive,
+    })
+
     }
 }
-
-
 
 // Result<(), Box<dyn error::Error>>
 // is a good pick for the run() function !
@@ -34,11 +55,29 @@ pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
 
     // read_to_string returns a Result
     let contents = fs::read_to_string(config.filename)?;
-    let results = search(&config.query, &contents);
+
+    // UGLY CODE: OTHER WAY TO AVOID SCOPE ERR FOR RESULTS BELOW
+    // if config.case_sensitive {
+    //     let results = search(&config.query, &contents);
+    //     for line in results {
+    //         println!("{}", line);
+    //     }
+    // }
+    // else {
+    //     let results = search_case_insensitive(&config.query, &contents);
+    //     for line in results {
+    //         println!("{}", line);
+    //     }
+    // }
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    
     for line in results {
         println!("{}", line);
     }
-    // println!("With text:\n{}", contents);
 
     Ok(())
 }
@@ -59,7 +98,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     found
 }
 
-
+// Add search_case_insensitive() function with TDD
+pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    // search(&query.to_lowercase(), &contents.to_lowercase()).copy()
+    let mut found = vec![];
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            found.push(line);
+        }
+    }
+    found
+}
 
 // Approach used here TDD = Test-Driven Development
 // Testing
@@ -76,9 +125,37 @@ safe, fast, productive.
 Pick three.";
 
         // Assert the line we expect to contain the term is the right one.
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents));
         // We need to create the search function, otherwhise the test won't
         // compile and we can't even watch it fail..
     }
-}
 
+    // this test tests the same as one_result()
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+
+    assert_eq!(
+        vec!["safe, fast, productive."],
+        search(query, contents));
+    
+    }
+    
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,28 @@
 // >rustgrep(main)$ grep print src/main.rs
 //println!("{:?}",args);                                  println!("Searching for {} in file {}", query, filename);
 
-
+use rustgrep::{run, Config};
 use std::env; // allows to collect cmd line args
-use std::process;
-use rustgrep::{Config, run}; //lib.rs
+use std::process; //lib.rs
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // get args:
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        // eprintln! prints to stderr instead of stdout !
+        // cargo run > output.txt
+        // will write error to the terminal,
+        // while stdoutput will be written to output.txt
+        // output.txt will remain empty in this case.
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     // println!("Searching for {} in file {}", config.query, config.filename);
 
     // run(config); with error handling
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     } // if let instead of unwrap_or_else()
-
 }
